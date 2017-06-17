a15fdf19bc341598f08170d325f00365f4737bc4
jevoncode <jevoncode@gmail.com>
2017-06-17 17:41:45 +0800

    feat: 高效而不繁琐的实例化泛型集合

---
+++ src/main/java/com/jc/generic/util/New.java
@@ -0,0 +1,47 @@
+package com.jc.generic.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+/**
+ * 高效而不繁琐的实例化泛型集合
+ * 反例:Map<Person, List<? extends Pet>> petPeople = new HashMap<Person, List<? extends Pet>>();
+ * @author jevoncode
+ *
+ */
+public class New {
+	public static <K, V> Map<K, V> map() {
+		return new HashMap<K, V>();
+	}
+
+	public static <T> List<T> list() {
+		return new ArrayList<T>();
+	}
+
+	public static <T> LinkedList<T> lList() {
+		return new LinkedList<T>();
+	}
+
+	public static <T> Set<T> set() {
+		return new HashSet<T>();
+	}
+
+	public static <T> Queue<T> queue() {
+		return new LinkedList<T>();
+	}
+
+	// Examples:
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Map<String, List<String>> sls = New.map();
+		List<String> ls = New.list();
+		LinkedList<String> lls = New.lList();
+		Set<String> ss = New.set();
+		Queue<String> qs = New.queue();
+	}
+}
