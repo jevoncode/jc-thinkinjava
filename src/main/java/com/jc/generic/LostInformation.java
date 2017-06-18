2f314eceb50f3b76afbd8760118b63ec208de02b
jevoncode <jevoncode@gmail.com>
2017-06-18 20:56:35 +0800

    feat: 泛型也称类型参数，但是使用getTypeParameters()获取不到，会被擦除，实际是object

---
+++ src/main/java/com/jc/generic/LostInformation.java
@@ -0,0 +1,39 @@
+package com.jc.generic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 泛型也称类型参数，但是使用getTypeParameters()获取不到，会被擦除，实际是object
+ * 
+ * @author jevoncode
+ *
+ */
+class Frob {
+}
+
+class Fnorkle {
+}
+
+class Quark<Q> {
+}
+
+class Particle<POSITION, MOMENTUM> {
+}
+
+public class LostInformation {
+	public static void main(String[] args) {
+		List<Frob> list = new ArrayList<Frob>();
+		Map<Frob, Fnorkle> map = new HashMap<Frob, Fnorkle>();
+		Quark<Fnorkle> quark = new Quark<Fnorkle>();
+		Particle<Long, Double> p = new Particle<Long, Double>();
+		System.out.println(Arrays.toString(list.getClass().getTypeParameters()));
+		System.out.println(Arrays.toString(map.getClass().getTypeParameters()));
+		System.out.println(Arrays.toString(quark.getClass().getTypeParameters()));
+		System.out.println(Arrays.toString(p.getClass().getTypeParameters())); 
+	}
+}
+ 
