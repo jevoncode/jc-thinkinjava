94bf2233ca735dd399434d5a4316300d35dae585
jevoncode <jevoncode@gmail.com>
2017-06-19 11:54:30 +0800

    feat: 【泛型】展示对创建对象new的补偿，引入一个工厂对象即可
    既然ClassTypeCapture这个例子展示了对instance of的补偿。那这个例子则展示对创建对象new的补偿，引入一个工厂对象即可

--- src/main/java/com/jc/generic/InstantiateGenericType.java
+++ src/main/java/com/jc/generic/InstantiateGenericType.java
@@ -1,7 +1,7 @@
 package com.jc.generic;
 
 /**
- * 既然ClassTypeCapture这个例子展示了对instance of的补偿。那这个例子则展示对创建对象new的补偿
+ * 既然ClassTypeCapture这个例子展示了对instance of的补偿。那这个例子则展示对创建对象new的补偿，引入一个工厂对象即可
  *
  */
 class ClassAsFactory<T> {
