de283e89843a884326c0cc085f827e0fe2065428
jevoncode <jevoncode@gmail.com>
2017-06-22 20:03:00 +0800

    feat: 数组和泛型的关系，实例化时不能直接使用泛型，但可以参数化数组的类型

---
+++ src/main/java/com/jc/arrays/ParameterizedArrayType.java
@@ -0,0 +1,29 @@
+package com.jc.arrays;
+
+/**
+ * 数组和泛型的关系，实例化时不能直接使用泛型，但可以参数化数组的类型
+ * @author jevoncode
+ * 
+ */
+class ClassParameter<T> {
+	public T[] f(T[] arg) {
+		return arg;
+	}
+}
+
+class MethodParameter {
+	public static <T> T[] f(T[] arg) {
+		return arg;
+	}
+}
+
+public class ParameterizedArrayType {
+	public static void main(String[] args) {
+		Integer[] ints = { 1, 2, 3, 4, 5 };
+		Double[] doubles = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+		Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+		Double[] doubles2 = new ClassParameter<Double>().f(doubles);
+		ints2 = MethodParameter.f(ints);
+		doubles2 = MethodParameter.f(doubles);
+	}
+}
