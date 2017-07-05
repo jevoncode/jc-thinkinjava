ce384c18b9cb8db984078b4d505add83746eefca
jevoncode <jevoncode@gmail.com>
2017-07-05 10:32:02 +0800

    feat: 新IO，简单的使用FileChannel和ByteBuffer的例子

---
+++ src/main/java/com/jc/io/GetChannel.java
@@ -0,0 +1,37 @@
+package com.jc.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+/**
+ * 新IO，简单的使用FileChannel和ByteBuffer的例子
+ * 
+ * 读的时候，这个buff.flip()显得略拙劣，但却是能获取最大的IO速度
+ * @author jevoncode
+ *
+ */
+public class GetChannel {
+	private static final int BSIZE = 1024;
+
+	public static void main(String[] args) throws Exception {
+		// Write a file:
+		FileChannel fc = new FileOutputStream("data.txt").getChannel();
+		fc.write(ByteBuffer.wrap("Some text ".getBytes()));
+		fc.close();
+		// Add to the end of the file:
+		fc = new RandomAccessFile("data.txt", "rw").getChannel();
+		fc.position(fc.size()); // Move to the end
+		fc.write(ByteBuffer.wrap("Some more".getBytes()));
+		fc.close();
+		// Read the file:
+		fc = new FileInputStream("data.txt").getChannel();
+		ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+		fc.read(buff);
+		buff.flip();
+		while (buff.hasRemaining())
+			System.out.print((char) buff.get());
+		fc.close();
+	}
+}
