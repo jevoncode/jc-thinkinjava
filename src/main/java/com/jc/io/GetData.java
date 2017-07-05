a881e57dd1fc7b762647d9b7b2f3ac5d47d93e2f
jevoncode <jevoncode@gmail.com>
2017-07-05 18:10:24 +0800

    feat: ByteBuffer对基本数据类型的处理

---
+++ src/main/java/com/jc/io/GetData.java
@@ -0,0 +1,52 @@
+package com.jc.io;
+
+import java.nio.ByteBuffer;
+
+/**
+ * ByteBuffer对基本数据类型的处理
+ * 
+ * ByteBuffer的初始化值都为0
+ * @author jevoncode
+ *
+ */
+public class GetData {
+	private static final int BSIZE = 1024;
+
+	public static void main(String[] args) {
+		ByteBuffer bb = ByteBuffer.allocate(BSIZE);
+		// Allocation automatically zeroes the ByteBuffer:
+		int i = 0;
+		while (i++ < bb.limit())
+			if (bb.get() != 0)
+				System.out.println("nonzero");
+		System.out.println("i = " + i);
+		bb.rewind();
+		// Store and read a char array:
+		bb.asCharBuffer().put("Howdy!");
+		char c;
+		while ((c = bb.getChar()) != 0)
+			System.out.print(c + " ");
+		System.out.println();
+		bb.rewind();
+		// Store and read a short:
+		bb.asShortBuffer().put((short) 471142);
+		System.out.println(bb.getShort());
+		bb.rewind();
+		// Store and read an int:
+		bb.asIntBuffer().put(99471142);
+		System.out.println(bb.getInt());
+		bb.rewind();
+		// Store and read a long:
+		bb.asLongBuffer().put(99471142);
+		System.out.println(bb.getLong());
+		bb.rewind();
+		// Store and read a float:
+		bb.asFloatBuffer().put(99471142);
+		System.out.println(bb.getFloat());
+		bb.rewind();
+		// Store and read a double:
+		bb.asDoubleBuffer().put(99471142);
+		System.out.println(bb.getDouble());
+		bb.rewind();
+	}
+}
