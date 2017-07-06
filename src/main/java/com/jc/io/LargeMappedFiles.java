6b30b7a334e1f460ba9ac6b05a9f53ad8bbdc238
jevoncode <jevoncode@gmail.com>
2017-07-06 10:57:46 +0800

    feat: 大文件使用MappedByteBuffer进行读写

---
+++ src/main/java/com/jc/io/LargeMappedFiles.java
@@ -0,0 +1,24 @@
+package com.jc.io;
+
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 大文件使用MappedByteBuffer进行读写
+ * @author jevoncode
+ *
+ */
+public class LargeMappedFiles {
+	static int length = 0x8FFFFFF; // 128 MB
+
+	public static void main(String[] args) throws Exception {
+		MappedByteBuffer out = new RandomAccessFile("test.dat", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE,
+				0, length);
+		for (int i = 0; i < length; i++)
+			out.put((byte) 'x');
+		System.out.println("Finished writing");
+		for (int i = length / 2; i < length / 2 + 6; i++)
+			System.out.print((char) out.get(i));
+	}
+}
