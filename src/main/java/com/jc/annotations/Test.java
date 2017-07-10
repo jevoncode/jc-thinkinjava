497e8fe765848e9b0d1a4c0ce6f9af0828988720
jevoncode <jevoncode@gmail.com>
2017-07-10 19:03:24 +0800

    feat: 注解，也被称为元数据（metadata）

--- src/main/java/com/jc/annotations/Test.java
+++ src/main/java/com/jc/annotations/Test.java
@@ -8,7 +8,8 @@ import java.lang.annotation.Target;
 /**
  * 注解，也被称为元数据（metadata）
  * @author jevoncode
- *
+ *  @Target定义应用到类的什么位置
+ *  @Retention定义哪个级别可以使用，有源代码（SOURCE）、类文件（CLASS）或者是运行时（RUNTIME）
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
