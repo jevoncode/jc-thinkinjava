843e51f215119fe52ceec3f46c66277c9ceab908
jevoncode <jevoncode@gmail.com>
2017-07-16 22:32:25 +0800

    feat: 多线程可以用于响应式设计中

---
+++ src/main/java/com/jc/concurrency/ResponsiveUI.java
@@ -0,0 +1,38 @@
+package com.jc.concurrency;
+
+/**
+ * 多线程可以用于响应式设计中
+ * @author jevoncode
+ *
+ */
+class UnresponsiveUI {
+	private volatile double d = 1;
+
+	public UnresponsiveUI() throws Exception {
+		while (d > 0)
+			d = d + (Math.PI + Math.E) / d;
+		System.in.read(); // Never gets here
+	}
+}
+
+public class ResponsiveUI extends Thread {
+	private static volatile double d = 1;
+
+	public ResponsiveUI() {
+		setDaemon(true);
+		start();
+	}
+
+	public void run() {
+		while (true) {
+			d = d + (Math.PI + Math.E) / d;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		// ! new UnresponsiveUI(); // Must kill this process
+		new ResponsiveUI();
+		System.in.read();
+		System.out.println(d); // Shows progress
+	}
+}
