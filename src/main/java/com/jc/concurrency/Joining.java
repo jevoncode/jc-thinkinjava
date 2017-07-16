d140d112a9ee15cc03377d2254c91f266c86ffb8
jevoncode <jevoncode@gmail.com>
2017-07-16 22:29:29 +0800

    feat: 一个线程可以等待一个线程完成，那就是用join

---
+++ src/main/java/com/jc/concurrency/Joining.java
@@ -0,0 +1,52 @@
+package com.jc.concurrency;
+/**
+ * 一个线程可以等待一个线程完成，那就是用join
+ * @author jevoncode
+ *
+ */
+class Sleeper extends Thread {
+	private int duration;
+
+	public Sleeper(String name, int sleepTime) {
+		super(name);
+		duration = sleepTime;
+		start();
+	}
+
+	public void run() {
+		try {
+			sleep(duration);
+		} catch (InterruptedException e) { //异常捕获时会将Interrupted这个标志位重置为false，所以在这里输出false
+			System.out.println(getName() + " was interrupted. " + "isInterrupted(): " + isInterrupted());
+			return;
+		}
+		System.out.println(getName() + " has awakened");
+	}
+}
+
+class Joiner extends Thread {
+	private Sleeper sleeper;
+
+	public Joiner(String name, Sleeper sleeper) {
+		super(name);
+		this.sleeper = sleeper;
+		start();
+	}
+
+	public void run() {
+		try {
+			sleeper.join();
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted");
+		}
+		System.out.println(getName() + " join completed");
+	}
+}
+
+public class Joining {
+	public static void main(String[] args) {
+		Sleeper sleepy = new Sleeper("Sleepy", 1500), grumpy = new Sleeper("Grumpy", 1500);
+		Joiner dopey = new Joiner("Dopey", sleepy), doc = new Joiner("Doc", grumpy);
+		grumpy.interrupt();
+	}
+}
