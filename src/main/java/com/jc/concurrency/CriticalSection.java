563a30598244d504382138605276e1e6886311be
jevoncode <jevoncode@gmail.com>
2017-07-29 14:03:45 +0800

    feat: 同步方法和同步块的性能对比，也只是在某些场合下才突出同步快的优势

---
+++ src/main/java/com/jc/concurrency/CriticalSection.java
@@ -0,0 +1,176 @@
+package com.jc.concurrency;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+/**
+ * 同步方法和同步块的性能对比，也只是在某些场合下才突出同步快的优势
+ * Pair不是线程安全所以需通过PairManager去操作Pair类
+ * 使用模版方式展示不同的同步方法，一个是方法同步，另一个是同步控制块
+ * 实验结果，PairManager2肯定比PairManager1快。
+ * @author jevoncode
+ *
+ */
+class Pair { // Not thread-safe
+	private int x, y;
+
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Pair() {
+		this(0, 0);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void incrementX() {
+		x++;
+	}
+
+	public void incrementY() {
+		y++;
+	}
+
+	public String toString() {
+		return "x: " + x + ", y: " + y;
+	}
+
+	public class PairValuesNotEqualException extends RuntimeException {
+		public PairValuesNotEqualException() {
+			super("Pair values not equal: " + Pair.this);
+		}
+	}
+
+	// Arbitrary invariant -- both variables must be equal:
+	public void checkState() {
+		if (x != y)
+			throw new PairValuesNotEqualException();
+	}
+}
+
+// Protect a Pair inside a thread-safe class:
+abstract class PairManager {
+	AtomicInteger checkCounter = new AtomicInteger(0);
+	protected Pair p = new Pair();
+	private List<Pair> storage = Collections.synchronizedList(new ArrayList<Pair>());
+
+	public synchronized Pair getPair() {
+		// Make a copy to keep the original safe:
+		return new Pair(p.getX(), p.getY());
+	}
+
+	// Assume this is a time consuming operation
+	protected void store(Pair p) {
+		storage.add(p);
+		
+		
+		/**如果注释此块会让pm1比pm2快**/
+		/**
+		 * 因为如果注释这块，那么同步时锁时间的差就差不多了。  pm2就是靠着这store方法的延迟才清清楚楚的战胜pm1
+		 */
+		try {
+			TimeUnit.MILLISECONDS.sleep(50);
+		} catch (InterruptedException ignore) {
+		}
+		/**如果注释此块会让pm1比pm2快**/
+		
+	}
+
+	public abstract void increment();
+}
+
+// Synchronize the entire method:
+class PairManager1 extends PairManager {
+	public synchronized void increment() {
+		p.incrementX();
+		p.incrementY();
+		store(getPair());
+	}
+}
+
+// Use a critical section:
+class PairManager2 extends PairManager {
+	public void increment() {
+		Pair temp;
+		synchronized (this) {
+			p.incrementX();
+			p.incrementY();
+			temp = getPair();
+		}
+		store(temp);
+	}
+}
+
+class PairManipulator implements Runnable {
+	private PairManager pm;
+
+	public PairManipulator(PairManager pm) {
+		this.pm = pm;
+	}
+
+	public void run() {
+		while (true)
+			pm.increment();
+	}
+
+	public String toString() {
+		return "Pair: " + pm.getPair() + " checkCounter = " + pm.checkCounter.get();
+	}
+}
+
+/**
+ * 
+ * 检查类
+ *
+ */
+class PairChecker implements Runnable {
+	private PairManager pm;
+
+	public PairChecker(PairManager pm) {
+		this.pm = pm;
+	}
+
+	public void run() {
+		while (true) {
+			pm.checkCounter.incrementAndGet();
+			pm.getPair().checkState();
+		}
+	}
+}
+
+public class CriticalSection {
+	// Test the two different approaches:
+	static void testApproaches(PairManager pman1, PairManager pman2) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		PairManipulator pm1 = new PairManipulator(pman1), pm2 = new PairManipulator(pman2);
+		PairChecker pcheck1 = new PairChecker(pman1), pcheck2 = new PairChecker(pman2);
+		exec.execute(pm1);
+		exec.execute(pm2);
+		exec.execute(pcheck1);
+		exec.execute(pcheck2);
+		try {
+			TimeUnit.MILLISECONDS.sleep(500);
+		} catch (InterruptedException e) {
+			System.out.println("Sleep interrupted");
+		}
+		System.out.println("pm1: " + pm1 + "\npm2: " + pm2);
+		System.exit(0);
+	}
+
+	public static void main(String[] args) {
+		PairManager pman1 = new PairManager1(), pman2 = new PairManager2();
+		testApproaches(pman1, pman2);
+	}
+}
